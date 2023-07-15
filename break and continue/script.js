2aaff858fa74c38a27cffd8b5a544f8f85fef93e
aldoprogrammer <107610584+aldoprogrammer@users.noreply.github.com>
2023-07-15 07:33:12 +0700

    Add files via upload

---
+++ break and continue/script.js
@@ -0,0 +1,17 @@
+// break and continue
+
+// continue untuk melangkahi
+// break untuk berhenti secara total
+for (let i =0; i <=20; i++) {
+    if(i == 13){
+    continue;
+    }
+    console.log("nilai dari continu dilewati ",i);
+}
+
+for (let i =0; i <=20; i++) {
+    if(i == 13){
+    break;
+    }
+    console.log("nilai break berhenti total ", i);
+}
