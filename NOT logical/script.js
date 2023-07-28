b966aca74901b32f5dbb7696170ef10fc7cacb03
aldoprogrammer <107610584+aldoprogrammer@users.noreply.github.com>
2023-07-28 08:57:49 +0700

    Add files via upload

---
+++ NOT logical/script.js
@@ -0,0 +1,17 @@
+// kalo bolean kita ga mesti harus bikin tanda kurung
+// kalo bukan bolean kita mesti bikin tanda kurung
+
+let temp = 15;
+let sunny = true;
+
+if (!(temp > 0)){
+    console.log("It's cold outside")
+} else {
+    console.log("It's warm outside!")
+}
+
+if (!sunny) {
+    console.log("Its a sunny day")
+} else {
+    console.log("It's cloudy")
+}
